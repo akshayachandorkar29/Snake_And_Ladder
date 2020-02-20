d08367fc2fbd645caddc5a33b9778a441fcd994c
Akshaya <akshayachandorkar29@gmail.com>
2020-02-20 16:46:16 +0530

    Added: generated random option and accordingly moved player_position

--- snake_and_ladder.sh
+++ snake_and_ladder.sh
@@ -12,9 +12,38 @@ player_position=0
 start_position=0
 win_position=100
 
+#USE CASE 2
 #Rolling a Dice
 function die_roll()
 {
 	dice=$(( RANDOM%6 + 1 ))
 	echo $dice
 }
+
+#USE CASE 3
+#random options
+function random_option()
+{
+	option=$(( RANDOM%3 ))
+	echo $option
+}
+
+#option 1 = No PLAY
+#option 2 = LADDER
+#option 3 = SNAKE
+#changing position according to option value
+case $(random_option) in
+	0)
+		player_position=$player_position
+		;;
+	1)
+		player_position=$(( $player_position + $(die_roll) ))
+		;;
+	2)
+		player_position=$(( $player_position - $(die_roll) ))
+		;;
+	*)
+		echo "Invalid Option"
+		;;
+esac
+
