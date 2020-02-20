ec975206422d8a74d7cc4e4d61e1ba4ec0b5adb2
Akshaya <akshayachandorkar29@gmail.com>
2020-02-20 15:37:28 +0530

    Added: defined player position and set constants

--- snake_and_ladder.sh
+++ snake_and_ladder.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "---WELCOME TO THE WORLD OF SNAKE AND LADDER SIMULATOR---"
+
+#USE CASE 1
+#taking user input
+read -p "Enter Username: " username
+echo Welcome $username
+
+#constants
+player_position=0
+start_position=0
+win_position=0
